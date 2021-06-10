eadd78c79b3994aa296be8bbfedcd490332427f4
Oleh <my.mail.olech@gmail.com>
2021-06-10 11:11:00 +0300

    added open/close event for dropdown

---
+++ events/dropdown.js
@@ -0,0 +1,26 @@
+document
+  .querySelector(".select-wrapper")
+  .addEventListener("click", function () {
+    this.querySelector(".select").classList.toggle("open");
+  });
+
+for (const option of document.querySelectorAll(".option")) {
+  option.addEventListener("click", function () {
+    if (!this.classList.contains("selected")) {
+      this.parentNode
+        .querySelector(".option.selected")
+        .classList.remove("selected");
+      this.classList.add("selected");
+      this.closest(".select").querySelector(
+        ".select__trigger span"
+      ).textContent = this.textContent;
+    }
+  });
+}
+
+window.addEventListener("click", function (e) {
+  const select = document.querySelector(".select");
+  if (!select.contains(e.target)) {
+    select.classList.remove("open");
+  }
+});
